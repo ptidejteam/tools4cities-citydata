c3021b39f5af428110e17eb3562b57d4b158d9cd
Rushin-Makwana <rushymakwana@gmail.com>
2025-12-03 14:19:42 -0500

    solved failed test error

--- Middleware/src/test/java/ca/concordia/encs/citydata/core/DatastoreManagerTest.java
+++ Middleware/src/test/java/ca/concordia/encs/citydata/core/DatastoreManagerTest.java
@@ -1,26 +1,18 @@
 package ca.concordia.encs.citydata.core;
 
+import ca.concordia.encs.citydata.core.configs.AppConfig;
+import ca.concordia.encs.citydata.core.contracts.IDatastoreManager;
+import ca.concordia.encs.citydata.datastores.DatastoreManager;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
 
-import ca.concordia.encs.citydata.PayloadFactory;
-import ca.concordia.encs.citydata.TestTokenGenerator;
-
-import ca.concordia.encs.citydata.core.configs.AppConfig;
-import ca.concordia.encs.citydata.datastores.DatastoreManager;
-import ca.concordia.encs.citydata.core.contracts.IDatastoreManager;
-
-import static org.junit.jupiter.api.Assertions.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-import static org.hamcrest.Matchers.*;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 /**
 * DatastoreManager test to check existing datastores.

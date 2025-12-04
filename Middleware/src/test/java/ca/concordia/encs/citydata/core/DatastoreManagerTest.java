43a8af2523ea8acf1a508ab7b4371093e86d3726
Gabriel Ullmann <gabriel.cavalheiroullmann@concordia.ca>
2025-12-03 20:05:01 -0500

    remove text leftover from merge

--- Middleware/src/test/java/ca/concordia/encs/citydata/core/DatastoreManagerTest.java
+++ Middleware/src/test/java/ca/concordia/encs/citydata/core/DatastoreManagerTest.java
@@ -9,7 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.test.web.servlet.MockMvc;<<<<<<< Add-homepage
+import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 
 import ca.concordia.encs.citydata.PayloadFactory;

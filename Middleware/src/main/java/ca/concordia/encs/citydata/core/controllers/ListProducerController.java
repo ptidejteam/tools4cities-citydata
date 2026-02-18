6e500aac2598d498168b99be63f8e172ed0175ab
Sikandar Ejaz <34721766+SikandarEjaz@users.noreply.github.com>
2026-02-17 23:28:46 -0500

    added base producers to the list of producers

--- Middleware/src/main/java/ca/concordia/encs/citydata/core/controllers/ListProducerController.java
+++ Middleware/src/main/java/ca/concordia/encs/citydata/core/controllers/ListProducerController.java
@@ -3,7 +3,9 @@ package ca.concordia.encs.citydata.core.controllers;
 import java.io.File;
 import java.lang.reflect.Method;
 import java.nio.file.Paths;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,6 +23,7 @@ import ca.concordia.encs.citydata.core.utils.StringUtils;
  * @author Sikandar Ejaz
  * @since 2025-06-02
  */
+
 @RestController
 @RequestMapping("/producers")
 public class ListProducerController {
@@ -28,39 +31,36 @@ public class ListProducerController {
 	@GetMapping("/list")
 	public String listProducers() {
 		final JsonArray producerDetailsList = new JsonArray();
-		// Get the path to the package
 		final String projectRootPath = Paths.get("").toAbsolutePath() + "/";
-		final String packagePath = projectRootPath + Constants.PRODUCER_ROOT_PACKAGE;
+
+		final Map<String, String> packagesToScan = new LinkedHashMap<>();
+		packagesToScan.put(projectRootPath + Constants.PRODUCER_ROOT_PACKAGE, "ca.concordia.encs.citydata.producers.");
+		packagesToScan.put(projectRootPath + Constants.PRODUCER_ROOT_PACKAGE + "base/",
+				"ca.concordia.encs.citydata.producers.base.");
 
 		try {
-			// Scan for class files in the package directory
 			final String fileExtension = ".java";
-			final File[] files = new File(packagePath).listFiles((dir, name) -> name.endsWith(fileExtension));
 
-			if (files != null) {
-				for (File file : files) {
-					// Remove .class extension
-					final String className = file.getName().replace(fileExtension, "");
+			for (Map.Entry<String, String> entry : packagesToScan.entrySet()) {
+				final String packagePath = entry.getKey();
+				final String classPrefix = entry.getValue();
 
-					// Load the class using reflection
-					final Class<?> clazz = Class.forName("ca.concordia.encs.citydata.producers." + className);
+				final File[] files = new File(packagePath).listFiles((dir, name) -> name.endsWith(fileExtension));
 
-					// Map to hold operation details
-					final JsonObject operationDetails = new JsonObject();
+				if (files != null) {
+					for (File file : files) {
+						final String className = file.getName().replace(fileExtension, "");
+						final Class<?> clazz = Class.forName(classPrefix + className);
 
-					// Set class name
-					operationDetails.addProperty("name", clazz.getName());
+						final JsonObject operationDetails = new JsonObject();
+						operationDetails.addProperty("name", clazz.getName());
 
-					// List setter methods, which correspond to user-accessible params
-					final Method[] methods = clazz.getMethods();
-					final List<String> paramList = StringUtils.getParamDescriptions(methods);
-					operationDetails.addProperty("params", String.join(", ", paramList));
-					producerDetailsList.add(operationDetails);
+						final Method[] methods = clazz.getMethods();
+						final List<String> paramList = StringUtils.getParamDescriptions(methods);
+						operationDetails.addProperty("params", String.join(", ", paramList));
+						producerDetailsList.add(operationDetails);
+					}
 				}
-			} else {
-				final JsonObject errorObject = new JsonObject();
-				errorObject.addProperty("error", "No files found in " + packagePath);
-				producerDetailsList.add(errorObject);
 			}
 
 		} catch (ClassNotFoundException e) {
@@ -70,6 +70,5 @@ public class ListProducerController {
 		}
 
 		return producerDetailsList.toString();
-
 	}
 }

bd565b21b3f7cf063b2248cc9ec6a6f09ec965eb
Sikandar Ejaz <34721766+SikandarEjaz@users.noreply.github.com>
2026-02-18 22:02:42 -0500

    Added new GeoJSONProducer

--- Middleware/src/main/java/ca/concordia/encs/citydata/producers/GeoJSONProducer.java
+++ Middleware/src/main/java/ca/concordia/encs/citydata/producers/GeoJSONProducer.java
@@ -1,7 +1,5 @@
 package ca.concordia.encs.citydata.producers;
 
-import java.security.InvalidParameterException;
-
 import ca.concordia.encs.citydata.core.contracts.IOperation;
 import ca.concordia.encs.citydata.core.contracts.IProducer;
 import ca.concordia.encs.citydata.core.contracts.IRunner;
@@ -9,19 +7,20 @@ import ca.concordia.encs.citydata.core.implementations.AbstractProducer;
 import ca.concordia.encs.citydata.producers.base.JSONProducer;
 
 /**
- * This Producer outputs metadata about a building, such as floors, zones and sensors.
- * @author Gabriel C. Ullmann
- * @since 2025-05-28
+ * This Producer outputs metadata about a building. 
+ * @author Sikandar Ejaz
+ * @since 2026-02-16
  */
 
-public class BuildingProducer extends AbstractProducer<String> implements IProducer<String> {
+public class GeoJSONProducer extends AbstractProducer<com.google.gson.JsonObject>
+		implements IProducer<com.google.gson.JsonObject> {
 	private JSONProducer jsonProducer;
 
-	public void setBuildingName(String buildingName) {
-		if (buildingName != null) {
-			jsonProducer = new JSONProducer("docs/examples/data/" + buildingName + "_building.json", null);
+	public void setFilePath(String filePath) {
+		if (filePath != null) {
+			jsonProducer = new JSONProducer(filePath, null);
 		} else {
-			throw new InvalidParameterException("Please provide a building name to the producer.");
+			throw new java.security.InvalidParameterException("Please provide a file path to the producer.");
 		}
 	}
 
@@ -40,5 +39,4 @@ public class BuildingProducer extends AbstractProducer<String> implements IProdu
 	public void addObserver(final IRunner aRunner) {
 		this.jsonProducer.addObserver(aRunner);
 	}
-
 }

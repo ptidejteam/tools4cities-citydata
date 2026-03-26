5279a3f3e4a95c0365d178c32f9c231b0c3e366a
Sikandar Ejaz <34721766+SikandarEjaz@users.noreply.github.com>
2026-03-26 14:03:52 -0400

    moved data to `src/test/resources`, issue #214

--- Middleware/src/test/java/ca/concordia/encs/citydata/test/producers/SanityProducerTest.java
+++ Middleware/src/test/java/ca/concordia/encs/citydata/test/producers/SanityProducerTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.Test;
 import com.google.gson.JsonObject;
 
 import ca.concordia.encs.citydata.producers.BuildingProducer;
+import ca.concordia.encs.citydata.producers.OccupancyProducer;
 
 public class SanityProducerTest {
 
@@ -16,6 +17,15 @@ public class SanityProducerTest {
 		producer.setBuildingName("mock");
 		producer.fetch();
 		ArrayList<JsonObject> result = producer.getResult();
+		System.out.println(result);
+	}
+
+	@Test
+	public void testOccupancyProducer() {
+		final OccupancyProducer producer = new OccupancyProducer();
+		producer.setListSize(0);
+		producer.fetch();
+		ArrayList<String> result = producer.getResult();
 		System.out.println(result);
 	}
 }

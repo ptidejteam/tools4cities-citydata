f161169ae94cddb94ad13bfc7111817d35bd5236
Sikandar Ejaz <34721766+SikandarEjaz@users.noreply.github.com>
2026-03-26 12:27:24 -0400

    addressed issue #214

---
+++ Middleware/src/test/java/ca/concordia/encs/citydata/test/core/DiscoveryRoutesTest.java
@@ -0,0 +1,73 @@
+package ca.concordia.encs.citydata.test.core;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.ComponentScan;
+
+import ca.concordia.encs.citydata.core.configs.AppConfig;
+import ca.concordia.encs.citydata.test.AbstractTest;
+import ca.concordia.encs.citydata.test.config.TestConfig;
+
+/**
+ * Discovery routes test
+ *
+ * @author Sikandar Ejaz
+ * @since 2025-06-18
+ */
+
+@SpringBootTest(classes = { AppConfig.class, TestConfig.class })
+@AutoConfigureMockMvc
+@ComponentScan(basePackages = "ca.concordia.encs.citydata.core")
+public class DiscoveryRoutesTest extends AbstractTest {
+
+	@Test
+	public void testRoutesList() throws Exception {
+		mockMvc.perform(get("/routes/list")).andExpect(status().isOk())
+				.andExpect(content().string(containsString("Method: [")));
+
+	}
+
+	@Test
+	public void testOperationsList() throws Exception {
+		mockMvc.perform(get("/operations/list")).andExpect(status().isOk())
+				.andExpect(content().string(containsString("ca.concordia.encs.citydata")));
+	}
+
+	@Test
+	public void testProducersList() throws Exception {
+		mockMvc.perform(get("/producers/list")).andExpect(status().isOk())
+				.andExpect(content().string(containsString("ca.concordia.encs.citydata")));
+	}
+
+	@Test
+	void testListOperationsController() throws Exception {
+		mockMvc.perform(get("/operations/list")).andExpect(status().is2xxSuccessful())
+				.andExpect(content().string(containsString("ca.concordia.encs.citydata.operations.MergeOperation")))
+				.andExpect(content().string(containsString("targetProducerParams")))
+				.andExpect(content().string(containsString("targetProducer")));
+	}
+
+	@Test
+	void testListProducerController() throws Exception {
+		mockMvc.perform(get("/producers/list")).andExpect(status().is2xxSuccessful())
+				.andExpect(content()
+						.string(containsString("ca.concordia.encs.citydata.producers.EnergyConsumptionProducer")))
+				.andExpect(content().string(containsString("operation")))
+				.andExpect(content().string(containsString("city")));
+	}
+
+	@Test
+	void testRouteController() throws Exception {
+		mockMvc.perform(get("/routes/list")).andExpect(status().is2xxSuccessful()).andExpect(jsonPath("$").isArray())
+				.andExpect(jsonPath("$[?(@ =~ /.*\\/operations\\/list.*/)]").exists())
+				.andExpect(content().string(containsString("/operations/list")))
+				.andExpect(content().string(containsString("Method: [GET]")));
+	}
+}
---
+++ Middleware/src/test/java/ca/concordia/encs/citydata/test/producers/GeometryProducerTest.java
@@ -0,0 +1,53 @@
+package ca.concordia.encs.citydata.test.producers;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.http.MediaType;
+
+import ca.concordia.encs.citydata.core.configs.AppConfig;
+import ca.concordia.encs.citydata.operations.MergeOperation;
+import ca.concordia.encs.citydata.producers.EnergyConsumptionProducer;
+import ca.concordia.encs.citydata.producers.GeometryProducer;
+import ca.concordia.encs.citydata.test.AbstractTest;
+import ca.concordia.encs.citydata.test.PayloadFactory;
+
+/**
+ * Tests the API endpoint with the merge operation between EnergyConsumption and Geometry producers
+ *
+ * @author Minette Zongo M., Sikandar Ejaz
+ * @since 2025-04-29 
+ */
+
+@SpringBootTest(classes = { AppConfig.class })
+@AutoConfigureMockMvc
+@ComponentScan(basePackages = "ca.concordia.encs.citydata.core")
+
+public class GeometryProducerTest extends AbstractTest {
+
+	private final String CITY = "montreal";
+
+	@BeforeEach
+	void setUp() {
+		GeometryProducer geometryProducer = new GeometryProducer();
+		EnergyConsumptionProducer energyConsumptionProducer = new EnergyConsumptionProducer();
+		MergeOperation mergeOperation = new MergeOperation();
+	}
+
+	@Test
+	public void testMergeOperationViaAPI() throws Exception {
+		// Get example query using the PayloadFactory
+		String jsonPayload = PayloadFactory.getExampleQuery("mergeEnergyConsumptionAndGeometries");
+
+		mockMvc.perform(post("/apply/sync").header("Authorization", "Bearer " + getToken())
+				.contentType(MediaType.APPLICATION_JSON).content(jsonPayload)).andExpect(status().isOk())
+				.andExpect(content().string(containsString("result")));
+	}
+}

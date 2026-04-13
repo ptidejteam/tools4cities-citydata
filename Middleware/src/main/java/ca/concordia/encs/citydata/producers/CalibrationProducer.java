b77bbb5d205d04cf672817b99f78bf029b16f8ca
Meyo-Ndo Z. <minettemeyo@gmail.com>
2026-04-13 14:05:32 -0400

    Added a calibration producer, a CVS filter operation and two Csv files for natural gas and electricity consumption for Calibration tests

---
+++ Middleware/src/main/java/ca/concordia/encs/citydata/producers/CalibrationProducer.java
@@ -0,0 +1,33 @@
+package ca.concordia.encs.citydata.producers;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import ca.concordia.encs.citydata.core.contracts.IProducer;
+import ca.concordia.encs.citydata.core.exceptions.MiddlewareException;
+import ca.concordia.encs.citydata.core.implementations.AbstractProducer;
+
+public class CalibrationProducer extends AbstractProducer<String> implements IProducer<String> {
+
+//	public void setFilePath(String fileName) {
+//        super.setFilePath("docs/examples/data/" + fileName);
+//    }
+	
+
+    @Override
+    public void fetch() {
+    	System.out.println("Fetching file from path: " + this.getFilePath());
+        try {
+            OutputStream outputStream = this.fetchFromPath();
+            String csvString = outputStream.toString();
+            ArrayList<String> csvLines = new ArrayList<>(Arrays.asList(csvString.split(System.lineSeparator())));
+            this.setResult(csvLines);
+            this.applyOperation();
+        } catch (Exception e) {
+            throw new MiddlewareException.DatasetNotFound("Error processing CSV data: " + e.getMessage());
+        }
+    }
+}
